c29a5999c2df21e01c0055167a7ab3bff082a46e
Daniel <marcosdanielsferreira2015@gmail.com>
2026-08-31 10:36:28 -0300

    Criação de menu principal

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace Gastos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("==== CONTROLE DE GASTO =====");
+            Console.WriteLine("1 - Adicionar gasto");
+            Console.WriteLine("2 - Listar gastos");
+            Console.WriteLine("3 - Buscar gasto");
+            Console.WriteLine("4 - Remover gasto");
+            Console.WriteLine("5 - Ver total gasto");
+            Console.WriteLine("6 - Ver maior gasto");
+            Console.WriteLine("7 - Relatório por categória");
+            Console.WriteLine("0 - Sair");
+
+            Console.Write("Escolha uma opção: ");
+            int menu = int.Parse(Console.ReadLine() ?? "");
+
+
+            switch (menu)
+            {
+                case 1:
+                  
+                break;
+            }
+
+
+
+        }
+    }
+}
